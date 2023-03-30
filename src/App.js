c80fed9e26796d3ad824552c67f181808fa28a48
ayar07 <saktanov0720@gmail.com>
2023-03-30 17:18:53 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Title(props) {
+  console.log(props);
+  return (
+    <>
+      <h1>{props.text}</h1>
+      <h1>{props.title}</h1>
+      <p>{props.test}</p>
+      <p>{props.url}</p>
+
+    </>
+  )
+}
+
+
+function Header() {
+  return (
+    <>
+      <Title title="header content" />
+    </>
+  )
+}
+
+
+function Body() {
+  return (
+    <>
+      <Title title="BoDy" />
+    </>
+  )
+}
+
+
+function Footer(props) {
+  return (
+    <>
+      <p>
+        <Title text="some text" url="22222" />
+      </p>
+    </>
+  )
+}
+
+
+function Inner() {
+  return (
+    <>
+      <Title test="content inner" />
+    </>
+  )
+}
+
+
+
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Title />
+        <Footer />
+        <Body />
+        <Inner />
+      </div>
+
     </div>
   );
 }
